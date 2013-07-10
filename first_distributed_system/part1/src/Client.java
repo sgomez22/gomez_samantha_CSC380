2b7e9ada0ff551eb5aa15155d69ec047f0854215
Samantha Gomez <sgomez@student.neumont.edu>
2013-07-09 18:01:21 -0600

    Initial commit of part one

---
+++ first_distributed_system/part1/src/Client.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sgomez
+ * Date: 7/9/13
+ * Time: 5:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
---
+++ first_distributed_system/part1/src/MathLogic.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sgomez
+ * Date: 7/9/13
+ * Time: 5:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MathLogic {
+
+    public int add(int a, int b){
+       return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+}
