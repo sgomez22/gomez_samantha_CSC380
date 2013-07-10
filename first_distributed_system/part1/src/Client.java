e554b4ad99fc0a1d4e7bd51323eab5f1520af497
Samantha Gomez <sgomez@student.neumont.edu>
2013-07-09 21:47:49 -0600

    Can now add and subtract, but will not allow multiple users at this time.

--- first_distributed_system/part1/src/Client.java
+++ first_distributed_system/part1/src/Client.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: sgomez
@@ -6,4 +14,59 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Client {
+
+    public static void main(String[] args) {
+        new Client().run();
+    }
+
+    public void run(){
+        Scanner scan = new Scanner(System.in);
+        String numbers = null;
+        boolean running = true;
+        while(running){
+            try{
+                System.out.println("[1] Add \n[2] Subtract \n[3] Exit");
+                int input =  scan.nextInt();
+                scan.nextLine();
+
+                switch(input){
+                    case 1:
+                        System.out.println("Enter two numbers to add, separated by a space");
+                        numbers = scan.nextLine();
+                        break;
+                    case 2:
+                        System.out.println("Enter two numbers to subtract, separated by a space");
+                        numbers = scan.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("Good bye!");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Please enter numbers 1 or 2");
+                        break;
+                }
+
+                Socket server = new Socket("localhost", 30000);
+                OutputStream serverOut = server.getOutputStream();
+                InputStream serverIn = server.getInputStream();
+
+                PrintWriter serverWrite = new PrintWriter(serverOut, true);
+                serverWrite.println(numbers);
+                serverWrite.println(input);
+                server.shutdownOutput();
+
+                Scanner serverScan = new Scanner(serverIn);
+                serverScan.useDelimiter("$");
+                String resp = serverScan.next();
+
+                System.out.println(resp);
+                server.shutdownInput();
+                server.close();
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
 }
+
--- first_distributed_system/part1/src/Server.java
+++ first_distributed_system/part1/src/Server.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created with IntelliJ IDEA.
  * User: sgomez
@@ -6,4 +12,35 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Server {
+
+    public static void main(String[] args) {
+        MathLogic mathlogic = new MathLogic();
+        try{
+            ServerSocket server = new ServerSocket(30000);
+            System.out.println("Connecting to client...");
+            Socket client = server.accept();
+
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String numbers = in.readLine();
+            String input = in.readLine();
+            String[] splitNumbers = numbers.split(" ");
+            int a = Integer.valueOf(splitNumbers[0]);
+            int b = Integer.valueOf(splitNumbers[1]);
+            int value = Integer.valueOf(input);
+
+            if(value == 1){
+                out.println(mathlogic.add(a, b));
+            } else if(value == 2){
+                out.println(mathlogic.subtract(a, b));
+            }
+
+            out.close();
+            in.close();
+            client.close();
+            server.close();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
